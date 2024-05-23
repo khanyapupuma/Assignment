fc90ed9602a599f050e74933576ebc0442226b78
khanyapupuma <khalilpupuma@gmail.com>
2024-05-23 10:29:20 +0200

    Assignment

---
+++ script/code.js
@@ -0,0 +1,22 @@
+
+function greet(word){
+    console.log( "Hello" + word )
+}
+greet("Khanya")
+
+function multipy(numb1,numb2){
+ let result = numb1*numb2
+ console.log(result)
+}
+multipy(7,4)
+
+function tableOfFive(numb5){
+    let result = numb5*5
+console.log(result)
+          return result
+}
+tableOfFive(3)
+tableOfFive(5)
+tableOfFive(7)
+tableOfFive(2)
+tableOfFive(0)
